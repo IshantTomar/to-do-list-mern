9085cb8ea369c2edc41b280e2bd255e7ad63dc17
Ishant Tomar <ishanttomar26@gmail.com>
2026-02-05 12:52:52 +0530

    Added createNote

--- Backend/src/controllers/notesControllers.js
+++ Backend/src/controllers/notesControllers.js
@@ -1,13 +1,34 @@
+import Note from "../models/Note.js";
+
 export async function getAllNotes(req, res) {
-    res.status(200).json({ message: "working" });
+    try {
+        const notes = await Note.find().sort({ createdAt: -1 });
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getallNotes controller.", error);
+        res.status(500).json({ message: "Internal Server error." });
+    }
 }
 
 export async function getNoteById(req, res) {
-    res.status(200).json({ message: "working" });
+    try {
+        const { title, content } = req.body;
+    } catch (error) {
+        console.error("Error in getNoteById controller.", error);
+        res.status(500).json({ message: "Internal Server error." });
+    }
 }
 
 export async function createNote(req, res) {
-    res.status(201).json({ message: "working" });
+    try {
+        const { title, content } = req.body;
+        const note = new Note({ title, content });
+        const savednote = await note.save();
+        res.status(201).json(savednote);
+    } catch (error) {
+        console.error("Error in createNote controller.", error);
+        res.status(500).json({ message: "Internal Server error." });
+    }
 }
 
 export async function updateNote(req, res) {
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001
 
 connectDB();
 
-express.json();
+app.use(express.json());
 
 app.use("/api/notes", router);
 
